7513a4ca2df61eeb977ec5e77c9d193dbf524669
Timothy Axford <t.axford2@yahoo.com>
2023-03-21 11:52:11 -0400

    Added an alternative example to assign to a jaggedArray

--- JaggedArrays.Net5.0/Program.cs
+++ JaggedArrays.Net5.0/Program.cs
@@ -16,6 +16,14 @@ namespace JaggedArrays.Net5._0
             jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
             jaggedArray[1] = new int[] { 1, 2, 3 };
             jaggedArray[2] = new int[] { 13, 21 };
+
+            // alternative way
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[] { 2, 3, 5, 7, 11 },
+                new int[] { 1, 2, 3 },
+                new int[] { 13, 21 }
+            };
         }
     }
 }
