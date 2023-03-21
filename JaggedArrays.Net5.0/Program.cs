e91096912ac53491b4f018a04475d3e35dd28999
Timothy Axford <t.axford2@yahoo.com>
2023-03-21 11:49:49 -0400

    Assigned values in the jaggedArray

--- JaggedArrays.Net5.0/Program.cs
+++ JaggedArrays.Net5.0/Program.cs
@@ -8,6 +8,14 @@ namespace JaggedArrays.Net5._0
         {
             // declare jaggedArray
             int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
         }
     }
 }
